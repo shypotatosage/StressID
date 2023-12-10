5c9676c5c304ccb236f6b5b3e44a574569a9ab0f
CallistaEve <eyaurentius@gmail.com>
2023-12-10 17:16:40 +0700

    Main Activity

--- app/src/main/java/com/imtuc/stressid/view/MainActivity.kt
+++ app/src/main/java/com/imtuc/stressid/view/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.TextField
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -58,7 +63,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun inputData(predictionViewModel: PredictionViewModel, lifecycleOwner: LifecycleOwner, navController: NavController) {
+fun inputData(
+    predictionViewModel: PredictionViewModel,
+    lifecycleOwner: LifecycleOwner,
+    navController: NavController
+) {
     val context = LocalContext.current
 
     var anxietyLevel = remember {
@@ -121,210 +130,211 @@ fun inputData(predictionViewModel: PredictionViewModel, lifecycleOwner: Lifecycl
     var bullying = remember {
         mutableStateOf("")
     }
-
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        TextField(
-            value = anxietyLevel.value,
-            onValueChange = { anxietyLevel.value = it },
-            label = { Text("Anxiety Level") },
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = selfEsteem.value,
-            onValueChange = { selfEsteem.value = it },
-            label = { Text("Self Esteem Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = mentalHealthHistory.value,
-            onValueChange = { mentalHealthHistory.value = it },
-            label = { Text("Mental Health Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = depression.value,
-            onValueChange = { depression.value = it },
-            label = { Text("Depression Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = headache.value,
-            onValueChange = { headache.value = it },
-            label = { Text("Headache Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = bloodPressure.value,
-            onValueChange = { bloodPressure.value = it },
-            label = { Text("bloodPressure Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = sleepQuality.value,
-            onValueChange = { sleepQuality.value = it },
-            label = { Text("sleepQuality Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = breathingProblem.value,
-            onValueChange = { breathingProblem.value = it },
-            label = { Text("breathingProblem Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = noiseLevel.value,
-            onValueChange = { noiseLevel.value = it },
-            label = { Text("noiseLevel Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = livingConditions.value,
-            onValueChange = { livingConditions.value = it },
-            label = { Text("livingConditions Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = safety.value,
-            onValueChange = { safety.value = it },
-            label = { Text("safety Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = basicNeeds.value,
-            onValueChange = { basicNeeds.value = it },
-            label = { Text("basicNeeds Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = academicPerformance.value,
-            onValueChange = { academicPerformance.value = it },
-            label = { Text("academicPerformance Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = studyLoad.value,
-            onValueChange = { studyLoad.value = it },
-            label = { Text("studyLoad Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = teacherStudentRelationship.value,
-            onValueChange = { teacherStudentRelationship.value = it },
-            label = { Text("teacherStudentRelationship Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = futureCareerConcerns.value,
-            onValueChange = { futureCareerConcerns.value = it },
-            label = { Text("futureCareerConcerns Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = socialSupport.value,
-            onValueChange = { socialSupport.value = it },
-            label = { Text("socialSupport Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = peerPressure.value,
-            onValueChange = { peerPressure.value = it },
-            label = { Text("peerPressure Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = extracurricularActivities.value,
-            onValueChange = { extracurricularActivities.value = it },
-            label = { Text("extracurricularActivities Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        TextField(
-            value = bullying.value,
-            onValueChange = { bullying.value = it },
-            label = { Text("bullying Level") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            TextField(
+                value = anxietyLevel.value,
+                onValueChange = { anxietyLevel.value = it },
+                label = { Text("Anxiety Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = selfEsteem.value,
+                onValueChange = { selfEsteem.value = it },
+                label = { Text("Self Esteem Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = mentalHealthHistory.value,
+                onValueChange = { mentalHealthHistory.value = it },
+                label = { Text("Mental Health Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = depression.value,
+                onValueChange = { depression.value = it },
+                label = { Text("Depression Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = headache.value,
+                onValueChange = { headache.value = it },
+                label = { Text("Headache Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = bloodPressure.value,
+                onValueChange = { bloodPressure.value = it },
+                label = { Text("bloodPressure Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = sleepQuality.value,
+                onValueChange = { sleepQuality.value = it },
+                label = { Text("sleepQuality Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = breathingProblem.value,
+                onValueChange = { breathingProblem.value = it },
+                label = { Text("breathingProblem Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = noiseLevel.value,
+                onValueChange = { noiseLevel.value = it },
+                label = { Text("noiseLevel Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = livingConditions.value,
+                onValueChange = { livingConditions.value = it },
+                label = { Text("livingConditions Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = safety.value,
+                onValueChange = { safety.value = it },
+                label = { Text("safety Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = basicNeeds.value,
+                onValueChange = { basicNeeds.value = it },
+                label = { Text("basicNeeds Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = academicPerformance.value,
+                onValueChange = { academicPerformance.value = it },
+                label = { Text("academicPerformance Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = studyLoad.value,
+                onValueChange = { studyLoad.value = it },
+                label = { Text("studyLoad Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = teacherStudentRelationship.value,
+                onValueChange = { teacherStudentRelationship.value = it },
+                label = { Text("teacherStudentRelationship Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = futureCareerConcerns.value,
+                onValueChange = { futureCareerConcerns.value = it },
+                label = { Text("futureCareerConcerns Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = socialSupport.value,
+                onValueChange = { socialSupport.value = it },
+                label = { Text("socialSupport Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = peerPressure.value,
+                onValueChange = { peerPressure.value = it },
+                label = { Text("peerPressure Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = extracurricularActivities.value,
+                onValueChange = { extracurricularActivities.value = it },
+                label = { Text("extracurricularActivities Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            TextField(
+                value = bullying.value,
+                onValueChange = { bullying.value = it },
+                label = { Text("bullying Level") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
 
 
 
-        Button(
-            onClick = {
-                predictionViewModel.predict(
+            Button(
+                onClick = {
+                    predictionViewModel.predict(
                         anxietyLevel.value.toInt(),
                         selfEsteem.value.toInt(),
-                    mentalHealthHistory.value.toInt(),
-                    depression.value.toInt(),
-                    headache.value.toInt(),
-                    bloodPressure.value.toInt(),
-                    sleepQuality.value.toInt(),
-                    breathingProblem.value.toInt(),
-                    noiseLevel.value.toInt(),
-                    livingConditions.value.toInt(),
-                    safety.value.toInt(),
-                    basicNeeds.value.toInt(),
-                    academicPerformance.value.toInt(),
-                    studyLoad.value.toInt(),
-                    teacherStudentRelationship.value.toInt(),
-                    futureCareerConcerns.value.toInt(),
-                    socialSupport.value.toInt(),
-                    peerPressure.value.toInt(),
-                    extracurricularActivities.value.toInt(),
-                    bullying.value.toInt(),
+                        mentalHealthHistory.value.toInt(),
+                        depression.value.toInt(),
+                        headache.value.toInt(),
+                        bloodPressure.value.toInt(),
+                        sleepQuality.value.toInt(),
+                        breathingProblem.value.toInt(),
+                        noiseLevel.value.toInt(),
+                        livingConditions.value.toInt(),
+                        safety.value.toInt(),
+                        basicNeeds.value.toInt(),
+                        academicPerformance.value.toInt(),
+                        studyLoad.value.toInt(),
+                        teacherStudentRelationship.value.toInt(),
+                        futureCareerConcerns.value.toInt(),
+                        socialSupport.value.toInt(),
+                        peerPressure.value.toInt(),
+                        extracurricularActivities.value.toInt(),
+                        bullying.value.toInt(),
 
 
-                    )
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        ) {
-            Text("Submit")
+                        )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text("Submit")
+            }
         }
     }
-}
+
 
 private fun validateInput(
     anxietyLevel: String,
@@ -343,14 +353,17 @@ fun ProvideWindowInsets(windowInsetsAnimationsEnabled: Boolean, content: () -> U
 @Composable
 fun GreetingPreview() {
     StressIDTheme {
-                navController = rememberNavController()
 
-                SetupNavGraph(
-                    navController = navController,
-                    lifecycleOwner = this
-                )
-            }
-        }
     }
-
 }
+//fun GreetingPreview() {
+//    StressIDTheme {
+//                navController = rememberNavController()
+//
+//                SetupNavGraph(
+//                    lifecycleOwner = this,
+//                    navController = navController,
+//
+//                )
+//            }
+//        }
