5dd3484c2ca33628d321869f5165c92f147ccbaa
shypotatosage <kathyjiegraal@gmail.com>
2023-12-10 16:08:15 +0700

    Update Front End Basics + Splash Screen

--- app/src/main/java/com/imtuc/stressid/view/MainActivity.kt
+++ app/src/main/java/com/imtuc/stressid/view/MainActivity.kt
@@ -10,39 +10,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.imtuc.stressid.navigation.SetupNavGraph
 import com.imtuc.stressid.ui.theme.StressIDTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StressIDTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                navController = rememberNavController()
+
+                SetupNavGraph(
+                    navController = navController,
+                    lifecycleOwner = this
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StressIDTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/imtuc/stressid/view/SplashScreenActivity.kt
@@ -0,0 +1,80 @@
+package com.imtuc.stressid.view
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.imtuc.stressid.R
+import com.imtuc.stressid.navigation.Screen
+import com.imtuc.stressid.ui.theme.Background
+import com.imtuc.stressid.ui.theme.StressIDTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreenActivity(navController: NavHostController) {
+    var startAnimation by remember {
+        mutableStateOf(false)
+    }
+
+    val alphaAnimation = animateFloatAsState(
+        targetValue = if (startAnimation) { 1f } else { 0f },
+        animationSpec = tween(
+            durationMillis = 2000
+        ), label = ""
+    )
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(2000)
+        navController.popBackStack()
+        navController.navigate(Screen.PredictionForm.route)
+    }
+
+    SplashScreen(alphaAnimation.value)
+}
+@Composable
+fun SplashScreen(alpha: Float) {
+    Column(modifier = Modifier.background(color = Background)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .alpha(alpha)
+                .padding(0.dp, 0.dp, 0.dp, 75.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "StressID Logo",
+                modifier = Modifier
+                    .width(250.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashPreview() {
+    StressIDTheme {
+        SplashScreen(1f)
+    }
+}
